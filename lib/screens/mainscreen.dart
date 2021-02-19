19c93fe88c853e2b6473c17a4fbd5d068d341017
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-20 01:13:50 +0600

    working on custom diyalog

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: Center(
+          child: RaisedButton(
+        child: Text("Select Your Language"),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (conetx) {
+                return Dialog();
+              });
+        },
+      )),
+    );
   }
 }
