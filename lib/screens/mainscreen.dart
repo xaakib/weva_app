bf88e7c16daee10ffb67d9e6321ded93869d2404
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-20 01:44:56 +0600

    custom diyalog ui done

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -11,7 +11,68 @@ class MainScreen extends StatelessWidget {
           showDialog(
               context: context,
               builder: (conetx) {
-                return Dialog();
+                return Center(
+                  child: Stack(
+                    overflow: Overflow.visible,
+                    alignment: Alignment.topCenter,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        height: 180,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(
+                            color: Colors.red, //
+                            width: 2.0,
+                          ),
+                        ),
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 70,
+                            ),
+                            Text(
+                              "Select Your language",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              "Select Your language",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        top: -40,
+                        child: Container(
+                          height: 80,
+                          width: 80,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                color: Colors.red, //
+                                width: 2.0,
+                              ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(100))),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
               });
         },
       )),
