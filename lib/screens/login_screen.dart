ae4c733462554cc5e06f105e01ed33a9709dc443
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-20 10:54:06 +0600

    login ui full done

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:international_phone_input/international_phone_input.dart';
+import 'package:weva_app/widgets/custom_btn.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -31,86 +32,106 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Container(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            SizedBox(
-              height: 30,
-            ),
-            Image.asset(
-              "assets/images/identify.jpg",
-              height: 200,
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            InternationalPhoneInput(
-              onPhoneNumberChange: onPhoneNumberChange,
-              initialPhoneNumber: phoneNumber,
-              initialSelection: phoneIsoCode,
-              decoration: InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 30,
               ),
-              enabledCountries: ['+880', ''],
-              labelText: "Phone Number",
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-                suffixIcon: Icon(Icons.lock_open_outlined),
+              Image.asset(
+                "assets/images/identify.jpg",
+                height: 200,
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    for (var i = 0; i < 1; i += 1)
-                      Row(
-                        children: [
-                          Checkbox(
-                            onChanged: i == 1
-                                ? null
-                                : (bool value) {
-                                    setState(() {
-                                      checked[i] = value;
-                                    });
-                                  },
-                            tristate: i == 1,
-                            value: checked[i],
-                            activeColor: Colors.red,
-                          ),
-                          Text(
-                            'Remember ',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
+              SizedBox(
+                height: 20,
+              ),
+              InternationalPhoneInput(
+                onPhoneNumberChange: onPhoneNumberChange,
+                initialPhoneNumber: phoneNumber,
+                initialSelection: phoneIsoCode,
+                decoration: InputDecoration(
+                  labelText: "Mobile Number",
+                  border: OutlineInputBorder(),
+                ),
+                enabledCountries: ['+880', ''],
+                labelText: "Phone Number",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.lock_open_outlined),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      for (var i = 0; i < 1; i += 1)
+                        Row(
+                          children: [
+                            Checkbox(
+                              onChanged: i == 1
+                                  ? null
+                                  : (bool value) {
+                                      setState(() {
+                                        checked[i] = value;
+                                      });
+                                    },
+                              tristate: i == 1,
+                              value: checked[i],
+                              activeColor: Colors.red,
                             ),
-                          ),
-                        ],
-                        mainAxisAlignment: MainAxisAlignment.center,
-                      ),
-                  ],
-                ),
-                Text(
-                  "Forget Password?",
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+                            Text(
+                              'Remember ',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        ),
+                    ],
                   ),
+                  Text(
+                    "Forget Password?",
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              CustomBtn(
+                text: "Login",
+                onPressed: () {
+                  print("Login");
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 16.0,
                 ),
-              ],
-            )
-          ]),
+                child: CustomBtn(
+                  text: "Create New Account",
+                  onPressed: () {
+                    print("Register");
+                  },
+                  outlineBtn: true,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
