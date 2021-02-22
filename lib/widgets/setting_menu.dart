86d329d4bdf4f55ee8d5fbcceb78d20645ede44d
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-22 17:29:03 +0600

    menu button done

---
+++ lib/widgets/setting_menu.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart ';
+
+class SettingsMenu extends StatelessWidget {
+  final String text;
+  final Function onpress;
+  final Icon icon;
+
+  const SettingsMenu({Key key, this.text, this.onpress, this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: icon,
+        title: Text(
+          text,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_right_sharp,
+            size: 40,
+            color: Colors.red,
+          ),
+        ));
+  }
+}
