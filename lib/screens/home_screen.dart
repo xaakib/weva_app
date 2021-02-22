34dfa0ef258baa90fe2e4f4ac195ee7dbec4dd42
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-22 19:12:36 +0600

    settings menu fiex

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:weva_app/screens/pages/health_screen.dart';
 import 'package:weva_app/screens/pages/home_product.dart';
 import 'package:weva_app/screens/pages/salon_screen.dart';
+import 'package:weva_app/widgets/setting_menu.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -34,10 +35,21 @@ class _HomeScreenState extends State<HomeScreen> {
                         fit: BoxFit.cover, height: 50, width: 70),
 
                     centerTitle: true,
-                    leading: Icon(
-                      Icons.menu,
-                      color: Colors.black,
-                      size: 30,
+                    leading: IconButton(
+                      onPressed: () {
+                        showDialog(
+                          barrierDismissible: true,
+                          context: context,
+                          builder: (conetx) {
+                            return showAlert();
+                          },
+                        );
+                      },
+                      icon: Icon(
+                        Icons.menu,
+                        color: Colors.black,
+                        size: 30,
+                      ),
                     ),
                     actions: [
                       IconButton(
@@ -140,6 +152,102 @@ class _HomeScreenState extends State<HomeScreen> {
         onTap: (index) {
           //Handle button tap
         },
+      ),
+    );
+  }
+
+  Widget showAlert() {
+    return AlertDialog(
+      content: Stack(
+        overflow: Overflow.visible,
+        alignment: Alignment.topCenter,
+        children: [
+          Container(
+            height: 500,
+            width: 300,
+            decoration: new BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: const Color(0xFFFFFF),
+              borderRadius: BorderRadius.all(Radius.circular(40.0)),
+            ),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SettingsMenu(
+                  icon: Icon(
+                    Icons.portable_wifi_off_outlined,
+                    color: Colors.green,
+                    size: 35,
+                  ),
+                  text: "Profile",
+                ),
+                Divider(),
+                SettingsMenu(
+                  icon: Icon(
+                    Icons.star,
+                    color: Colors.pink,
+                    size: 35,
+                  ),
+                  text: "Eva Points",
+                ),
+                Divider(),
+                SettingsMenu(
+                  icon: Icon(
+                    Icons.restore_outlined,
+                    color: Colors.purple,
+                    size: 35,
+                  ),
+                  text: "My reservation",
+                ),
+                Divider(),
+                SettingsMenu(
+                  icon: Icon(
+                    Icons.notification_important,
+                    color: Colors.green,
+                    size: 35,
+                  ),
+                  text: "Notification",
+                ),
+                Divider(),
+                SettingsMenu(
+                  icon: Icon(
+                    Icons.verified_user,
+                    color: Colors.red,
+                    size: 35,
+                  ),
+                  text: "User directory",
+                ),
+                Divider(),
+                SettingsMenu(
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.blue,
+                    size: 35,
+                  ),
+                  text: "About Us",
+                ),
+                Divider(),
+                SettingsMenu(
+                  icon: Icon(
+                    Icons.settings,
+                    color: Colors.red,
+                    size: 35,
+                  ),
+                  text: "Settings",
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+              top: -90,
+              child: Text(
+                "Menu",
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              )),
+        ],
       ),
     );
   }
--- lib/widgets/flight_categori.dart
+++ lib/widgets/flight_categori.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:weva_app/widgets/setting_menu.dart';
 
 class FlightCategori extends StatelessWidget {
   final String text;
@@ -12,15 +11,7 @@ class FlightCategori extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        showDialog(
-          barrierDismissible: true,
-          context: context,
-          builder: (conetx) {
-            return showAlert();
-          },
-        );
-      },
+      onTap: () {},
       child: Container(
         width: 70,
         child: Padding(
@@ -43,102 +34,6 @@ class FlightCategori extends StatelessWidget {
             ],
           ),
         ),
-      ),
-    );
-  }
-
-  Widget showAlert() {
-    return AlertDialog(
-      content: Stack(
-        overflow: Overflow.visible,
-        alignment: Alignment.topCenter,
-        children: [
-          Container(
-            height: 500,
-            width: 300,
-            decoration: new BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: const Color(0xFFFFFF),
-              borderRadius: BorderRadius.all(Radius.circular(40.0)),
-            ),
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                SettingsMenu(
-                  icon: Icon(
-                    Icons.portable_wifi_off_outlined,
-                    color: Colors.green,
-                    size: 35,
-                  ),
-                  text: "Profile",
-                ),
-                Divider(),
-                SettingsMenu(
-                  icon: Icon(
-                    Icons.star,
-                    color: Colors.pink,
-                    size: 35,
-                  ),
-                  text: "Eva Points",
-                ),
-                Divider(),
-                SettingsMenu(
-                  icon: Icon(
-                    Icons.restore_outlined,
-                    color: Colors.purple,
-                    size: 35,
-                  ),
-                  text: "My reservation",
-                ),
-                Divider(),
-                SettingsMenu(
-                  icon: Icon(
-                    Icons.notification_important,
-                    color: Colors.green,
-                    size: 35,
-                  ),
-                  text: "Notification",
-                ),
-                Divider(),
-                SettingsMenu(
-                  icon: Icon(
-                    Icons.verified_user,
-                    color: Colors.red,
-                    size: 35,
-                  ),
-                  text: "User directory",
-                ),
-                Divider(),
-                SettingsMenu(
-                  icon: Icon(
-                    Icons.search,
-                    color: Colors.blue,
-                    size: 35,
-                  ),
-                  text: "About Us",
-                ),
-                Divider(),
-                SettingsMenu(
-                  icon: Icon(
-                    Icons.settings,
-                    color: Colors.red,
-                    size: 35,
-                  ),
-                  text: "Settings",
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-              top: -90,
-              child: Text(
-                "Menu",
-                style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              )),
-        ],
       ),
     );
   }
