b0a5c82e55e1d27b8c368116238e5eb26711f632
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-03-17 13:15:03 +0600

    done

--- lib/screens/tab_screens/cardfile.dart
+++ lib/screens/tab_screens/cardfile.dart
@@ -4,190 +4,182 @@ class Cardfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: (Column(
-            children: <Widget>[
-              Row(
-                children: [
-                  IconButton(
-                      icon: Icon(
-                        Icons.arrow_back_ios,
-                        color: Colors.red,
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: (ListView(
+          children: <Widget>[
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 28.0),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSRV-8l-ZfPuMHcRP8rAXJXWGFhQelmO9qhw&usqp=CAU",
+                        ),
                       ),
-                      onPressed: () {}),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 28.0),
-                    child: Column(
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text("Hello Kitty Beauty spa"),
+                    Row(
                       children: [
-                        CircleAvatar(
-                          radius: 30,
-                          child: Image.asset("assets/images/capture.PNG"),
+                        Icon(Icons.add_location),
+                        Text("road-41/9,caffonia,USA"),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 28.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Clear Cut",
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Blow Dry",
+                          style: TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                        Text(
+                          "AED 250.00",
+                          style: TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red),
                         ),
                       ],
                     ),
                   ),
-                  Column(
-                    children: [
-                      Text("Hello Kitty Beauty spa"),
-                      Row(
-                        children: [
-                          Icon(Icons.add_location),
-                          Text("road-41/9,caffonia,USA"),
-                        ],
-                      ),
-                    ],
-                  )
+                  Spacer(),
+                  IconButton(icon: Icon(Icons.close), onPressed: () {}),
                 ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 28.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      "Clear Cut",
-                      style: TextStyle(
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                  ],
-                ),
+            ),
+            SizedBox(height: 30.0),
+            Container(
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.grey,
               ),
-              SizedBox(
-                height: 30,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Blow Dry",
+                          style: TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                        Text(
+                          "AED 250.00",
+                          style: TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  IconButton(icon: Icon(Icons.close), onPressed: () {}),
+                ],
               ),
-              Container(
-                height: 100,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 12.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Blow Dry",
-                            style: TextStyle(
-                                fontSize: 19,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black),
-                          ),
-                          Text(
-                            "AED 250.00",
-                            style: TextStyle(
-                                fontSize: 19,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.red),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Spacer(),
-                    IconButton(icon: Icon(Icons.close), onPressed: () {}),
-                  ],
-                ),
+            ),
+            SizedBox(height: 30.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 5.0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.add,
+                    color: Colors.green,
+                  ),
+                  Text(
+                    "ADD MORE SERVICE",
+                    style: TextStyle(color: Colors.green),
+                  ),
+                ],
               ),
-              SizedBox(height: 30.0),
-              Container(
-                height: 100,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 12.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Blow Dry",
-                            style: TextStyle(
-                                fontSize: 19,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black),
-                          ),
-                          Text(
-                            "AED 250.00",
-                            style: TextStyle(
-                                fontSize: 19,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.red),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Spacer(),
-                    IconButton(icon: Icon(Icons.close), onPressed: () {}),
-                  ],
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 38.0, right: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "AED 500.00",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+                  ),
+                ],
               ),
-              SizedBox(height: 30.0),
-              Padding(
-                padding: const EdgeInsets.only(left: 5.0),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.add,
-                      color: Colors.green,
-                    ),
-                    Text(
-                      "ADD MORE SERVICE",
-                      style: TextStyle(color: Colors.green),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 38.0, right: 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      "AED 500.00",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
-                    ),
-                  ],
-                ),
-              ),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 22.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.pink,
-                    ),
-                    height: 55.0,
-                    width: 250,
-                    // ignore: deprecated_member_use
-                    child: RaisedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Start Scheduling!",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 22.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.pink,
+                  ),
+                  height: 55.0,
+                  width: 250,
+                  // ignore: deprecated_member_use
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Start Scheduling!",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
                     ),
                   ),
                 ),
               ),
-            ],
-          )),
-        ),
+            ),
+          ],
+        )),
       ),
     );
   }
