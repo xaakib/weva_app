43d0ce106a7db21c4f3b750ebb5c233e97272b0a
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-03-19 23:54:32 +0600

    chnaged

--- lib/screens/bottom_NavigationScreens/profile_screen.dart
+++ lib/screens/bottom_NavigationScreens/profile_screen.dart
@@ -13,49 +13,62 @@ class Profile extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
+                  height: 150,
                   color: Color(0xffF8F8F8),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      SizedBox(
-                        height: 150,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Container(
-                          height: 105,
-                          width: 105,
-                          child: CircleAvatar(
-                            backgroundImage: AssetImage(
-                                "assets/images/profile-default_image.jpg"),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Container(
+                              height: 105,
+                              width: 105,
+                              child: CircleAvatar(
+                                backgroundImage: AssetImage(
+                                    "assets/images/profile-default_image.jpg"),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(3.0),
-                              child: Text(
-                                "Anna Franklin",
-                                style: TextStyle(
-                                    fontSize: 17,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold),
-                              ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(3.0),
+                                  child: Text(
+                                    "Anna Franklin",
+                                    style: TextStyle(
+                                        fontSize: 17,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(3.0),
+                                  child: Text(
+                                    "Eva point = 30",
+                                    style: TextStyle(
+                                        fontSize: 11, color: Colors.red),
+                                  ),
+                                ),
+                              ],
                             ),
-                            Padding(
-                              padding: const EdgeInsets.all(3.0),
-                              child: Text(
-                                "Eva point = 30",
-                                style:
-                                    TextStyle(fontSize: 11, color: Colors.red),
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
+                      Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blueAccent)),
+                          child: Icon(
+                            Icons.edit,
+                            size: 20,
+                          ))
                     ],
                   ),
                 ),
